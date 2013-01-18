38f8c0042608796301cb044b55ecc3499f19911d
Stefan Boesen <stefan.boesen@gmail.com>
2013-01-18 11:28:30 -0800

    Added ntlm support

--- cracker.py
+++ cracker.py
@@ -10,24 +10,31 @@ def get_sha256(word):
     encoded = base64.b64encode(hash)
     return str(encoded,'ascii')
 
-def hash_list(channel,target_hash,dictionary):
+def hash_list(channel,target_hash,dictionary, hash_type):
     import hashlib,base64
     hashes = []
     for i in range(len(dictionary)):
         word = dictionary[i].rstrip()
-        hash = hashlib.sha256(word.encode('utf-8')).digest()
-        encoded = base64.b64encode(hash)
-        final_hash = str(encoded)
-        hashes.append([word,final_hash])
+
+        if hash_type == 'ntlm':
+            hash = hashlib.new('md4', word.encode('utf-16le','ignore')).hexdigest()
+            hashes.append([word,"$NT$"+ hash])
+
+        elif hash_type == 'sha256':
+            hash = hashlib.sha256(word.encode('utf-8')).digest()
+            encoded = base64.b64encode(hash)
+            final_hash = str(encoded)
+            hashes.append([word,final_hash])
+
     channel.send(hashes)
 
-def master(gws,t,dictionary):
+def master(gws,t,dictionary, mode):
     #split dictionary
     parted = chunks(dictionary, round(len(dictionary)/len(gws)))
     node_data = zip(gws, list(parted))
     channels,result = [],[]
     for node, data in node_data:
-        channels.append(node.remote_exec(hash_list, target_hash = t, dictionary = data))
+        channels.append(node.remote_exec(hash_list, target_hash = t, dictionary = data, hash_type = mode))
     for c in channels: result += c.receive()
     return result
 
@@ -45,11 +52,12 @@ def main():
     
     gws = create_group('slacr.hosts')
 
-    result = master(gws, input_hash, lines)
+    result = master(gws, input_hash, lines, mode)
     for word, hash in result:
         if hash == input_hash:
             print("Found hash: " + hash + " word: " + word)
             return
+    print("Got input: " + input_hash)
     print("No hashes found. Look for yourself:")
     print(result)
 
